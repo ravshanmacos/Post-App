60ab49594bbdbd9d7ecf91fd444ced5deca51038
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2023-10-09 16:40:27 +0500

    Initial Commit

---
+++ ravshan_post_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ravshan_post_app
+//
+//  Created by Ravshan Winter on 09/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
